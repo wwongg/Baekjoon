05d8a11d9771cc2f30e654696072782b90b3dc4a
wwongg <nawonjin12@naver.com>
2024-02-29 23:39:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+        public static void main(String args[]) {
+            Scanner scan = new Scanner(System.in);
+            String input = scan.next();
+
+            int c = input.charAt(0);
+
+            System.out.println(c);
+
+
+
+    }
+}
