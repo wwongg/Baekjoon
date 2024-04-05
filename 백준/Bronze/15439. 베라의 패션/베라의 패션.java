f5c904d10756051de6606e48542167a01a9e4e71
wwongg <nawonjin12@naver.com>
2024-04-05 20:24:29 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int m = scan.nextInt();
+        
+
+        System.out.println(m * (m-1));
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
