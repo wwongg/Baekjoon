c0cf240a10940c56dd447fa475e00d0c09adf942
wwongg <nawonjin12@naver.com>
2024-06-24 19:37:54 +0900

    [Silver IV] Title: 카드2, Time: 244 ms, Memory: 21668 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,46 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        int front = 1;
+        int back = N;
+
+        int[] card = new int[2 * N];
+
+        for (int i = 1; i <= N; i++) {
+            card[i] = i;
+            back = i;
+        }
+
+        for(int i = N; i > 1; i--) {
+            front++;
+
+            card[back + 1] = card[front];
+
+            back++;
+            front++;
+        }
+
+        System.out.println(card[front]);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
