cf45149ab9dc61880797f6ab6f8d60b185dccf1b
wwongg <nawonjin12@naver.com>
2024-08-01 16:50:34 +0900

    [Bronze I] Title: 2진수 8진수, Time: 2104 ms, Memory: 91944 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        BigInteger N = new BigInteger(s, 2);
+        
+        String result = N.toString(8);
+
+        System.out.println(result);
+
+
+    }
+
+    // 배열 뒤집기
+
+    
+}
