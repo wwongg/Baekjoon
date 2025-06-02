68226024db5a4244c2658113757f802a77e78486
wwongg <nawonjin12@naver.com>
2025-06-02 18:05:16 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,63 @@
 
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Queue;
+
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        Queue<Pair> que = new LinkedList<>();
-        int n = scan.nextInt(); // 행
-        int m = scan.nextInt();  // 열
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        int[][] dist = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
 
-        char[][] maze = new char[n][m];
-        String str;
-        for (int i = 0; i < n; i++) {
-            str = scan.next();
-            for (int j = 0; j < m; j++) {
-                maze[i][j] = str.charAt(j);
-                dist[i][j] = -1;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[][] maze = new int[y][x];
+
+        for (int i = 0; i < y; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < x; j++) {
+                int index = input.charAt(j) - '0';
+                maze[i][j] = index;
             }
         }
 
-        que.offer(new Pair(0, 0));
-        dist[0][0] = 0; // 0이 움직일 수 없는 방향
-        visited[0][0] = true;
+        boolean[][] visit = new boolean[y][x];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0});
+        visit[0][0] = true;
 
-        while (!que.isEmpty()) {
-            Pair p = que.poll();
-            for(int i = 0; i <4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n && maze[ny][nx] == '1' && !visited[ny][nx]) {
-                    dist[ny][nx] = dist[p.y][p.x] + 1;
-                    visited[ny][nx] = true;
-                    que.offer(new Pair(nx, ny));
+        while (!q.isEmpty()) {
+            int[] pair = q.poll();
+            int currentX = pair[1];
+            int currentY = pair[0];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = currentX + dx[i];
+                int ny = currentY + dy[i];
+
+                if (nx < 0 || nx >= x || ny < 0 || ny >= y) {
+                    continue;
+                } else {
+                    if(!visit[ny][nx] && maze[ny][nx] == 1) {
+                        visit[ny][nx] = true;
+                        maze[ny][nx] = maze[currentY][currentX] + 1;
+                        q.add(new int[]{ny, nx});
+                    }
                 }
-
             }
-        }
-
-        System.out.println(dist[n-1][m-1] + 1);
-    }
-
-
-        public static class Pair {
-            int x, y;
-            public Pair(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
 
         }
 
-
-
-
+        System.out.println(maze[y - 1][x - 1]);
     }
 
 
@@ -69,3 +66,14 @@ public class Main {
 
 
 
+
+
+
+
+
+   
+
+
+
+
+}
