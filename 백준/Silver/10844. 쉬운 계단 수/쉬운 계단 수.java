f3c35d5c80cbb7319801fa67ff1731cd718e98f0
wwongg <nawonjin12@naver.com>
2025-06-14 18:18:01 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,46 +1,47 @@
+
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
 
 public class Main {
 
-    static long[][] dp;
-    static long sum = 0;
+    static int[][] room;
+    final static long mod = 1000000000;
 
-    public static void main(String[] args) throws IOException{
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        dp = new long[N + 1][10];
+        long[][] dp = new long[n + 1][10];
 
-        for (int i = 0; i < 10; i++) {
+        // 오른쪽 맨 끝의 자릿수
+        for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
         }
 
+        for (int i = 2; i <= n; i++) {
 
-        for(int i = 1 ; i < 10 ; i++)
-            sum += stair(N,i);
-
-        System.out.println(sum%1000000000);
-
-
-    }
-
-    static long stair(int digit, int val) {
-        if(digit == 1) {
-            return dp[digit][val];
+            for (int j = 0; j < 10; j++) {
+                if(j == 0) {
+                    dp[i][0] = dp[i - 1][1] % mod;
+                }
+                else if(j == 9) {
+                    dp[i][9] = dp[i - 1][8] % mod;
+                }
+                else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
+                }
+            }
         }
 
-        if(dp[digit][val] == 0) {
-            if(val == 0) {
-                dp[digit][val] = stair(digit - 1, val + 1);
-            } else if(val ==9)
-                dp[digit][val] = stair(digit-1,val-1);
-            else
-                dp[digit][val] = stair(digit-1,val-1) + stair(digit-1,val+1);
+        long result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result += dp[n][i];
         }
 
-        return dp[digit][val]%1000000000;
+        System.out.println(result % mod);
 
     }
 
