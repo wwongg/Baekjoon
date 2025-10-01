a3f0ee14e4c9b57eba1e705a211743eacda365be
wwongg <nawonjin12@naver.com>
2025-10-01 12:56:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        long n = num;
+        
+        while(n != 1) {
+            
+            if(n % 2 == 0) {
+                n /= 2;
+                
+            } else {
+                n = n * 3 + 1;
+            }
+                answer++;
+            
+            
+            if(answer > 500) {
+                return -1;
+            }
+        }
+        
+            
+        return answer;
+    }
+}
